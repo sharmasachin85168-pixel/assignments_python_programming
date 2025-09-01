6a5f1118f508b584f1a56a08099cddc0a71e6e8a
sharmasachin85168-pixel <sharmasachin85168@gmail.com>
2025-09-01 14:34:36 +0530

    Add files via upload

---
+++ assignment_2.py
@@ -0,0 +1,20 @@
+#task_1
+n=int(input("enter the number"))
+if n%2==0:
+    print("the no. ",n," is even")
+else:
+    print("the no. ",n,"is odd number")
+print("thanks you")
+
+
+
+
+#task_2
+
+n=list(range(1,51))
+sum=0
+for i in n:
+    sum=sum+i
+print("the sum of first 50 natural no. is:",sum)
+
+   
