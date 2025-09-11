67b20e8b9013e842fd33a3b3623e9d6cec0300eb
sachin kumar <sharmasachin85168@gmail.com>
2025-09-11 15:23:41 +0530

    Update assignmnet_5.py

--- assignmnet_5.py
+++ assignmnet_5.py
@@ -1,29 +1,22 @@
-def task_1():
-    dict = {'sachin': 80, 'sumit': 79, 'rohit': 77, 'sattu': 50}
-    n = input("enter the student name :")
-    if n in dict:
-        print("student {} has {} marks".format(n, dict[n]))
-    else:
-        print("student {} not found".format(n))
-
-def task_2():
-    list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    list2 = []
-    list3 = []
-    for i in range(0, 5):
-        list2.append(list1[i])
-    n = len(list2)
-    for i in range(n - 1, -1, -1):
-        list3.append(list2[i])
-    print("orginal list : ", list1)
-    print("extracted first five elements : ", list2)
-    print("reversed extracted five elements : ", list3)
-
-
-n=int(input("enter the choice : \n 1.task_1 \n 2.task_2 \n"))
-if n==1:
-    task_1()
-elif n==2 :
-    task_2()
+#task_1
+dict = {'sachin': 80, 'sumit': 79, 'rohit': 77, 'sattu': 50}
+n = input("enter the student name :")
+if n in dict:
+    print("student {} has {} marks".format(n, dict[n]))
 else:
-    print("enter the valid choice :")
+    print("student {} not found".format(n))
+   
+#task_2
+list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+list2 = []
+list3 = []
+for i in range(0, 5):
+    list2.append(list1[i])
+n = len(list2)-1
+for i in range(n, -1, -1):
+    list3.append(list2[i])
+print("orginal list : ", list1)
+print("extracted first five elements : ", list2)
+print("reversed extracted five elements : ", list3)
+
+
